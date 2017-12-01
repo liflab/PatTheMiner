b8f7c8d37e67c293752816ae9ee52a051708b86c
Sylvain Hallé <shalle@acm.org>
2017-12-01 09:47:54 -0500

    Normalize

---
+++ Source/Core/src/ca/uqac/lif/cep/peg/Normalize.java
@@ -0,0 +1,49 @@
+package ca.uqac.lif.cep.peg;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import ca.uqac.lif.cep.functions.NothingToReturnException;
+import ca.uqac.lif.cep.functions.UnaryFunction;
+
+/**
+ * Normalizes a collection of numbers
+ * @author Sylvain Hallé
+ */
+public class Normalize extends UnaryFunction<Object,Object> 
+{
+	public static final transient Normalize instance = new Normalize();
+	
+	protected Normalize()
+	{
+		super(Object.class, Object.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Object getValue(Object x) 
+	{
+		if (x instanceof Collection)
+		{
+			Collection<Number> l1 = (Collection<Number>) x;
+			List<Number> l2 = new ArrayList<Number>(l1.size());
+			float sum = 0;
+			for (Number n : l1)
+			{
+				sum += n.floatValue();
+			}
+			if (sum == 0)
+			{
+				throw new NothingToReturnException(this);
+			}
+			for (Number n : l1)
+			{
+				l2.add(n.floatValue() / sum);
+			}
+			return l2;
+		}
+		return null;
+	}
+
+}
