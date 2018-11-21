2ff3a48e9b07883d649ba0047bc22df19632a75a
Sylvain Hallé <shalle@acm.org>
2018-11-21 11:12:25 +0100

    Illustrations and text for the two trend patterns in Javadoc

--- Source/Core/src/ca/uqac/lif/cep/peg/SelfCorrelatedTrendDistance.java
+++ Source/Core/src/ca/uqac/lif/cep/peg/SelfCorrelatedTrendDistance.java
@@ -36,6 +36,96 @@ import ca.uqac.lif.cep.tmf.Trim;
 import ca.uqac.lif.cep.tmf.Window;
 
 /**
+ * Compares a trend calculated on a window events to another trend computed on
+ * another window placed further away in the past. Graphically, this generic
+ * pattern can be represented as follows:
+ * <p>
+ * <img src="{@docRoot}/doc-files/SCTD-1-chain.png" alt="Self-correlated trend distance">
+ * <p>
+ * The processor can also be seen as a "black box" taking four parameters, and
+ * in this case is represented as follows:
+ * <p>
+ * <img src="{@docRoot}/doc-files/SCTD-1.png" alt="Self-correlated trend distance">
+ * <p>
+ * The parameters are:
+ * <table>
+ * <tr><th align="center">Parameter</th><th>Description</th></tr>
+ * <tr>
+ *   <td align="center">n</td>
+ *   <td>The width of each window ("past" and "present")</td>
+ * </tr>
+ * <tr>
+ *   <td align="center">m</td>
+ *   <td>The amount of offset between the "past" and the "present" window</td>
+ * </tr>
+ * <tr>
+ *   <td align="center"><img src="{@docRoot}/doc-files/BetaProcessor.png" alt="&beta;"></td>
+ *   <td>The processor used to compute the trend on both windows</td>
+ * </tr>
+ * <tr>
+ *   <td align="center"><img src="{@docRoot}/doc-files/DistanceFunction.png" alt="&delta;"></td>
+ *   <td>The metric used to compute the distance between the two trends</td>
+ * </tr> 
+ * </table>
+ * <p>
+ * Optionally, the pattern can be instantiated with two more parameters: a
+ * comparison function and a threshold metric. The pattern is extended to become
+ * the following:
+ * <p>
+ * <img src="{@docRoot}/doc-files/SCTD-2-chain.png" alt="Self-correlated trend distance">
+ * <p>
+ * Represented as a single box, this becomes:
+ * <p>
+ * <img src="{@docRoot}/doc-files/SCTD-2.png" alt="Self-correlated trend distance">
+ * <p>
+ * The additional parameters are:
+ * <table>
+ * <tr><th align="center">Parameter</th><th>Description</th></tr>
+ * <tr>
+ *   <td align="center">d</td>
+ *   <td>A distance threshold</td>
+ * </tr>
+ * <tr>
+ *   <td align="center"><img src="{@docRoot}/doc-files/ComparisonFunction.png" alt="Comparison"></td>
+ *   <td>A function that compares the distance between trends to d, and
+ *   determines if the distance "exceeds" the threshold</td>
+ * </tr>
+ * </table>
+ * <p>
+ * Let us explain this workflow. Boxes #1–3 and #5–6 in the previous diagram
+ * are similar to the static {@link TrendDistance} pattern. The difference lies
+ * upstream, in how the “current”
+ * and the “reference” trends are extracted from the input event
+ * sequence. That sequence is first split into two copies (box
+ * #9). The topmost copy is trimmed of its first m events, as
+ * sis represented by box #7. This makes such that the streams
+ * entering boxes #1 and #4 are offset by m: while box #4 receives
+ * the stream of events e₀ , e₁, &hellip;, box #1 receives the stream
+ * e<sub>m</sub>, e<sub>m+1</sub>, &hellip; These two boxes then apply the same
+ * computation β on a sliding window: box #1 on a window of width n, and
+ * box #4 on a window of width m. The output of β on these two
+ * windows is then sent to the distance metric (box #3), and the
+ * rest of the process unfolds similarly to the static trend distance
+ * workflow we have introduced earlier.
+ * <p>
+ * As before, the distance metric (box #3) is fed with a
+ * sequence of pairs of trends (p, t), where t ∈ T is the trend
+ * computed on the latest window of n events. The reference
+ * trend p ∈ P, however, is now also computed from a window
+ * of events of the same stream. Let k be the number of events
+ * received from the input stream so far, with k ≥ m + n. Due
+ * to the presence of the trimming box (#7), it can be observed
+ * that, when box #1 applies β on a window of the last n events
+ * ({e<sub>k−(n−1)</sub>, e<sub>k−(n−2)</sub>, &hellip;, e<sub>k</sub>})
+ *  box #4 applies β on a window of the m preceding events 
+ * ({e<sub>k−n−(m−1)</sub>, e<sub>k−n−(m−2)</sub>, &hellip;, e<sub>k−n</sub>s}).
+ * In other words, the distance metric compares the trend computed
+ * from the last n events to a reference computed from the m
+ * events before them. The rest works in a similar way to the static trend
+ * distance workflow.
+ * <p>
+ * The first variant is simply missing the boxes #5 and #6; rather than
+ * comparing the distance to a threshold, it outputs the distance directly.
  * 
  * @param <P> The type of the pattern
  * @param <Q> The type returned by the beta processor
--- Source/Core/src/ca/uqac/lif/cep/peg/TrendDistance.java
+++ Source/Core/src/ca/uqac/lif/cep/peg/TrendDistance.java
@@ -41,6 +41,56 @@ import ca.uqac.lif.cep.tmf.Window;
  * <p>
  * <img src="{@docRoot}/doc-files/TrendDistance.png" alt="TrendDistance">
  * <p>
+ * First, a stream of events is received and sent into a sliding
+ * window computation (box #1 in the diagram). Input events come
+ * in from the left, and new output events are produced and come
+ * out from the right of the box. Such a computation requires
+ * two parameters: the width of the window (labelled n), and
+ * a computation β to perform on each window, represented by
+ * box #2. Concretely, the sliding window computation creates
+ * a window of the first n events received (e<sub>0</sub>, 
+ * e<sub>1</sub>, &hellip;, e<sub>n−1</sub>). It
+ * then feeds this window of events to computation β; the output
+ * returned by β is the first event to be output by box #1. The
+ * computation then “slides” one event forward, and creates a new
+ * window made of events e<sub>1</sub> to e<sub>n</sub>. It feeds this window
+ * to β, whose return value is the second event to be output by box #1
+ * —and so on.
+ * <p>
+ * The end result is that, from an input stream of events, box
+ * #1 creates a new stream, made of the application of β on
+ * successive windows of width n. Intuitively, β is intended to
+ * represent the calculation of a “trend” on a window that slides
+ * over the input stream. Since the goal of the process is to detect
+ * whether the input stream exhibits a “deviation” from some
+ * norm, the computed trends will be compared to some reference.
+ * This is the purpose of box #3, which evaluates what is called
+ * the distance metric. It takes two arguments; the first is the
+ * stream of trends computed by box #1; the second is a stream of
+ * reference patterns, provided by box #4. In the simplest scenario,
+ * the reference pattern does not change throughout the whole
+ * input stream, and box #4 simply returns the same reference
+ * pattern P over and over.
+ * <p>
+ * For each pair (P, p), where P is the reference pattern and
+ * p is a pattern computed by box #1, the distance metric δ(P, p)
+ * is evaluated, and its value d p is returned as the output of box
+ * #3. Intuitively, δ is a function that estimates “how far” pattern
+ * p is from reference P. The workflow is expected to produce a
+ * notification when that distance becomes larger than a specific
+ * threshold. This is the task of box #6, which compares the
+ * computed distance d<sub>p</sub> with a fixed threshold value d (provided
+ * by box #5). The function v is called the comparison function:
+ * v(d<sub>p</sub>,d) returns T (true) when d<sub>p</sub> is “greater” than
+ * the maximum threshold value d.
+ * <p>
+ * The net effect is that the workflow receives a
+ * stream of arbitrary input events e<sub>0</sub>, e<sub>1</sub>, &hellip;,
+ * and produces as its output a sequence of Boolean values b<sub>0</sub>,
+ * b<sub>1</sub>, &hellip; Under normal
+ * conditions, this workflow outputs the value ⊥ (false) repeatedly.
+ * The occurrence of value T indicates an "anomaly".
+ * 
  * @author Sylvain Hallé
  *
  * @param <P> The type of the pattern
@@ -49,24 +99,24 @@ import ca.uqac.lif.cep.tmf.Window;
  */
 public class TrendDistance<P,Q,R> extends GroupProcessor
 {
-	/**
-	 * Instantiates a new trend distance processor.
-	 * @param pattern The reference pattern
-	 * @param n The width of the window
-	 * @param beta The processor that computes the trend on each window
-	 * @param delta The distance metric
-	 * @param d The maximum distance threshold
-	 * @param comp The comparison function between the computed distance
-	 *   and the maximum distance threshold
-	 */
-	public TrendDistance(P pattern, int n, Processor beta, Function delta, R d, BinaryFunction<R,R,Boolean> comp)
-	{
-		super(1, 1);
-		Window wp = new Window(beta, n);
-		build(wp, pattern, delta, d, comp);
-	}
-	
-	/**
+  /**
+   * Instantiates a new trend distance processor.
+   * @param pattern The reference pattern
+   * @param n The width of the window
+   * @param beta The processor that computes the trend on each window
+   * @param delta The distance metric
+   * @param d The maximum distance threshold
+   * @param comp The comparison function between the computed distance
+   *   and the maximum distance threshold
+   */
+  public TrendDistance(P pattern, int n, Processor beta, Function delta, R d, BinaryFunction<R,R,Boolean> comp)
+  {
+    super(1, 1);
+    Window wp = new Window(beta, n);
+    build(wp, pattern, delta, d, comp);
+  }
+
+  /**
    * Instantiates a new trend distance processor.
    * @param pattern The reference pattern
    * @param window A window processor
@@ -80,10 +130,10 @@ public class TrendDistance<P,Q,R> extends GroupProcessor
     super(1, 1);
     build(window, pattern, delta, d, comp);
   }
-	
-	protected void build(Processor window, P pattern, Function delta, R d, BinaryFunction<R,R,Boolean> comp)
-	{
-	  associateInput(INPUT, window, INPUT);
+
+  protected void build(Processor window, P pattern, Function delta, R d, BinaryFunction<R,R,Boolean> comp)
+  {
+    associateInput(INPUT, window, INPUT);
     ApplyFunction distance = new ApplyFunction(new FunctionTree(delta,
         new Constant(pattern),
         new StreamVariable(0)
@@ -96,5 +146,5 @@ public class TrendDistance<P,Q,R> extends GroupProcessor
     Connector.connect(distance, too_far);
     associateOutput(OUTPUT, too_far, OUTPUT);
     addProcessors(window, distance, too_far);
-	}
+  }
 }
